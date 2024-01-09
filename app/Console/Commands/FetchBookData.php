1fd11dc961edfeee61e9afcf66ba1121c1a385ad
fuuuu <sanchan.fuu@gmail.com>
2024-01-09 22:13:19 +0900

    Add GoogleBookService and implement searchBooks method

--- app/Console/Commands/FetchBookData.php
+++ app/Console/Commands/FetchBookData.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Services\GoogleBookService;
 use Illuminate\Console\Command;
 
 class FetchBookData extends Command
@@ -30,5 +31,17 @@ class FetchBookData extends Command
         $genre = $this->argument('genre');
 
         // Google Books APIから書籍情報を取得
+        // まずはserviceクラスをインスタンス化
+        $googleBookService = new GoogleBookService();
+        // serviceクラスのsearchBooksメソッドを実行
+        $books = $googleBookService->searchBooks($publisher, $genre);
+
+        // 今のところ、取得した書籍情報をコンソールに表示するだけ
+        // TODO: 万が一のエラーハンドリングを実装する
+        // TODO: Slackへの例外発生通知の実装のタイミングで、エラーハンドリングを実装する
+        dd($books);
+
+        // TODO: 取得した書籍情報をDBに保存する
+        // TODO: DBに保存するために、まずはBookモデルを作成する
     }
 }
